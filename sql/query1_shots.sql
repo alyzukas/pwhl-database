96dfa172fd7e42ab98cd8d3d9aff38d9a5e6209d
alyzukas <alyzukas@gmail.com>
2026-02-11 15:13:33 -0800

    Uploading Week 6 .sql files
    Updated Schema. Updated Sample Data. Multi-Table Queries. Week 6 JOIN Queries 1+2

---
+++ sql/query1_shots.sql
@@ -0,0 +1,70 @@
+/*This query pulls information about every shot: details about the shooter, the goalie, the blockers and assists when applicable. 
+We have detailed information about the shooter and the shot to perform analysis about the shooter
+we can compile data about shot efficiency, and analyze some basic defensive data from the blocker and goalie
+
+The following are requirements across the two submitted queries, for this query a 1 indiciates it is met a 0 indicates it is not met:
+1 - At least one INNER JOIN
+1 - At least one LEFT JOIN (to preserve rows and expose missing data)
+0 - WHERE filtering
+1 - ORDER BY sorting
+0 - At least one GROUP BY query with aggregation (COUNT, SUM, AVG, etc.)
+0 - At least one calculated column (examples: total_cost, completion_rate, price * quantity)*/
+
+SELECT t.team_code as shooting_team
+,gm.game_date
+,gm.duration
+-- shooter details
+,shooter.name as player_name
+,tn.position
+,shooter.shoots
+,shooter.hometown
+,shooter.birthdate
+,tn.jersey_number
+-- shot details
+,s.shot_key 
+,s.shot_type
+,s.shot_time
+,s.shot_quality
+-- goalie details
+,goalie.name as player_name2
+,s.is_goal
+--blocker and assists
+,blocker.name AS player_name3
+,primary_assist.name AS assist
+,secondary_assist.name as assist2
+--goal details
+,gl.is_power_play
+,gl.is_short_handed
+,gl.is_empty_net
+,gl.is_penalty_shot
+,gl.is_insurance_goal
+,gl.is_game_winning_goal
+,s.x_location
+,s.y_location
+FROM shot s
+INNER JOIN game gm 
+	on s.game_key=gm.game_key
+INNER JOIN player shooter 
+	on s.shooter_key=shooter.player_key
+INNER JOIN player goalie
+	on s.goalie_key=goalie.player_key
+LEFT JOIN player blocker
+	on s.blocker_key=blocker.player_key
+INNER JOIN tenure tn
+    ON shooter.player_key = tn.player_key
+   AND gm.season_key = tn.season_key
+INNER JOIN team t
+    ON tn.team_key = t.team_key
+LEFT JOIN goal gl 
+	ON s.game_key=gl.game_key
+    AND s.shot_key=gl.shot_key
+LEFT JOIN assist a 
+	ON gl.game_key=a.game_key
+	AND gl.goal_key=a.goal_key
+LEFT JOIN player primary_assist
+	ON a.player_key=primary_assist.player_key
+    and a.assist_number=1
+LEFT JOIN player secondary_assist
+	ON a.player_key=secondary_assist.player_key
+    and a.assist_number=2
+ORDER BY s.shot_key;
